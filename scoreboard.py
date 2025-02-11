15b148feed72db8dc1eebc2bb31a755ed89c996b
AnkitDotThakur <ankitlearningo@gmail.com>
2025-02-11 12:30:32 +0530

    For high score we are adding a txt file that will update the scoreboard.

--- scoreboard.py
+++ scoreboard.py
@@ -1,14 +1,15 @@
 from turtle import Turtle
-
 ALIHNMENT = "center"
 FONT = ("Arial", 24, "normal")
+
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("high_score.txt", mode="r") as high_score:
+            self.high_score = int(high_score.read())
         self.color("white")
         self.penup()
         self.goto(0, 250)
@@ -20,8 +21,10 @@ class Scoreboard(Turtle):
         self.write(f"Score = {self.score} High_Score = {self.high_score}", False, align=ALIHNMENT, font=FONT)
 
     def reset(self):
-        if self.score > self.high_score:
+        if self.score > int(self.high_score):
             self.high_score = self.score
+            with open("high_score.txt", mode="w") as high_score:
+                high_score.write(f"{self.score}")
         self.score = 0
         self.update_scoreboard()
 
